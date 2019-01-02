230b58c376a7da8bbff10be07ce1766b4c742ad1
Kelsey Holt <kelseyjo959@gmail.fm>
2019-01-02 11:19:52 -0600

    some axios functions barely working

--- pawws-front/src/components/screens/pets/Pets.js
+++ pawws-front/src/components/screens/pets/Pets.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
 export class Pets extends React.Component {
+  constructor(props) {
+		super(props);
+
+		this.state = {
+			responseArray: [],
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+	}
+
+  handleChange() {
+    console.log("I was clicked");
+    axios.get(`http://localhost:3000/getThings`, {
+    	headers: {
+    		"screenSize": window.innerHeight,
+    	},
+    }).then((res) => {
+    	this.setState({ responseArray: res.data });
+      });
+  }
+
+
   render() {
     return (
       <div className="pets">
         <h1>{ "Hello World" }</h1>
         <h1>{ "Hello Pets" }</h1>
-        <button>{"push to get pets!"}</button>
+        <button onClick={this.handleChange}>{"push to get pets!"}</button>
+        <div className="petList">
+          <li data={this.state.responseArray} />
+        </div>
       </div>
     );
   }
